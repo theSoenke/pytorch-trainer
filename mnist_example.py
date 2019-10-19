6976a7ef267ba15e9204ddf095c91b70da949be7
theSoenke <soenke.behrendt@gmail.com>
2019-10-19 12:34:33 +0200

    Add MNIST example script

---
+++ mnist_example.py
@@ -0,0 +1,96 @@
+import torch
+from model_checkpoint import ModelCheckpoint
+from module import Module
+from torch import nn
+from torch.functional import F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from trainer import Trainer
+
+
+class MNISTModel(Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+    def training_step(self, batch):
+        x, target = batch
+
+        output = self.forward(x)
+        loss = F.nll_loss(output, target)
+
+        logger = {'train_loss': loss}
+        return {'loss': loss, 'log': logger}
+
+    def validation_step(self, batch):
+        x, target = batch
+
+        output = self.forward(x)
+        loss = F.nll_loss(output, target)
+
+        logger = {'val_loss': loss}
+        return {'val_loss': loss, 'log': logger}
+
+    def validation_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        logs = {
+            'val_loss': avg_loss,
+        }
+        return {
+            'val_loss': avg_loss,
+            'log': logs,
+        }
+
+    def train_dataloader(self):
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+        dataset = MNIST(root='./mnist', train=True, transform=transform, download=True)
+        loader = DataLoader(
+            dataset=dataset,
+            batch_size=16,
+            shuffle=True,
+            num_workers=0
+        )
+
+        return loader
+
+    def val_dataloader(self):
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+        dataset = MNIST(root='./mnist', train=False, transform=transform, download=True)
+        loader = DataLoader(
+            dataset=dataset,
+            batch_size=16,
+            shuffle=False,
+            num_workers=0
+        )
+
+        return loader
+
+
+if __name__ == "__main__":
+    checkpoint_callback = ModelCheckpoint(
+        directory='./checkpoints',
+        monitor='val_loss',
+        save_best_only=True,
+        mode='min'
+    )
+    model = MNISTModel()
+    trainer = Trainer(checkpoint_callback=checkpoint_callback)
+    trainer.fit(model)
